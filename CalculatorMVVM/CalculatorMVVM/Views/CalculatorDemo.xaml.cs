e1a4db438a137d917d74e9738dded059bb3aa9a4
John <pierojbh@gmail.com>
2023-06-16 22:02:09 -0500

    primer commit

---
+++ CalculatorMVVM/CalculatorMVVM/Views/CalculatorDemo.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorMVVM.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculatorMVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalculatorDemo : ContentPage
+    {
+        public CalculatorDemo()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModels.CalculatorDemoViewModel();
+        }
+    }
+}
